88b2493d4ebff05f03ed3496dc59b820466422ba
Dzmitry Akulau <dzmitryakulau@dzmitrys-mbp.myfiosgateway.com>
2024-11-21 12:17:57 -0500

    env added

--- dbConnect.js
+++ dbConnect.js
@@ -1,14 +1,15 @@
-const mongoose=require('mongoose');
-//const { default: ErrorList } = require('antd/lib/form/ErrorList');
+const mongoose = require('mongoose');
+const MONGO_URL = process.env.MONGO_URL;
 
-const URL='mongodb+srv://Dmitry:0k6jwoplHeoNJ1fy@cluster0.zdaq5.mongodb.net/resumebuilder';
+mongoose.connect(MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 
-mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true});
-
-const connection=mongoose.connection;
-connection.on('connected',()=>{
-    console.log("Mongo DB is connected!")
-})
-connection.on("error",(error)=>{
-    console.log(error)
-}) 
+const connection = mongoose.connection;
+connection.on('connected', () => {
+    console.log('MongoDB is connected!');
+});
+connection.on('error', (error) => {
+    console.log(error);
+});
